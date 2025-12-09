f2f8d349c46145b44dfd1a42d371a5c0dc86f2f7
Milo Canseco <canseco3rd@gmail.com>
2025-12-09 04:50:05 -0700

    Created root route and mounted employees API.

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 import express from "express";
-const app = express();
-export default app;
+import employeesRouter from "./api/employees.js";
 
-// TODO: this file!
+const app = express();
+app.use(express.json());
+
+// Root route for testing server availability
+app.get("/", (req, res) => {
+  res.send("Welcome to the Fullstack Employees API.");
+});
+
+// Mount employees API
+app.use("/employees", employeesRouter);
+
+export default app;
